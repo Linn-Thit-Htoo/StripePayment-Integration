650349c02fdbd4f402ac0485b5fbd6a99c72acb3
Lin Thit Htoo <lth1212001@gmail.com>
2024-12-27 19:04:45 +0630

    -

--- StripePaymentExample/Program.cs
+++ StripePaymentExample/Program.cs
@@ -20,7 +20,6 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
