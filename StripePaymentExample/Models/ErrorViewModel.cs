76ef96e7e8ed77d8bfb91cf3c9bcd92e3e588854
Lin Thit Htoo <lth1212001@gmail.com>
2024-12-27 19:04:26 +0630

    -

--- StripePaymentExample/Models/ErrorViewModel.cs
+++ StripePaymentExample/Models/ErrorViewModel.cs
@@ -1,9 +1,8 @@
-namespace StripePaymentExample.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+namespace StripePaymentExample.Models;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
