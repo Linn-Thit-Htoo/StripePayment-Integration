fa8e729d4669152a7d3f05e4514b3452a03abdc5
Lin Thit Htoo <lth1212001@gmail.com>
2024-12-27 19:04:20 +0630

    -

--- StripePaymentExample/Models/CheckoutFormModel.cs
+++ StripePaymentExample/Models/CheckoutFormModel.cs
@@ -1,10 +1,9 @@
-﻿namespace StripePaymentExample.Models
+﻿namespace StripePaymentExample.Models;
+
+public class CheckoutFormModel
 {
-    public class CheckoutFormModel
-    {
-        public string? ProductName { get; set; }
-        public string? ProductDescription { get; set; }
-        public long Amount { get; set; }
-        public string? Currency { get; set; }
-    }
+    public string? ProductName { get; set; }
+    public string? ProductDescription { get; set; }
+    public long Amount { get; set; }
+    public string? Currency { get; set; }
 }
